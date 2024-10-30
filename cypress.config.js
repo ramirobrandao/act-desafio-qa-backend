ddb03db238a2ea017c9430096120ffdf41d9f2c3
Ramiro Brandão <ramirobrandao@outlook.com>
2024-10-29 23:37:05 -0300

    Adionando Mochawesome para geração de relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,20 @@ module.exports = defineConfig({
     },
     //acesso ao site/ambiente
     baseUrl: 'https://serverest.dev/',
+    reporter: 'mochawesome',
+    screenshotOnRunFailure: true,  //gera print quando um teste falhar
+    "reporterOptions": {
+      "reportDir": "cypress/reports/mochawesome-report",
+      "overwrite": false,
+      "html": true,
+      "json": true,
+      "charts": true,
+      "reportFilename": "report",
+      "timestamp": "mmddyyyy_HHMMss",
+      "inlineAssets": true,
+      "autoOpen": true,
+      "reportPageTitle": "Relatório de execução de testes backend",
+      "embeddedScreenshots": true  //integrar prints no relatório
+    }
   },
 });
